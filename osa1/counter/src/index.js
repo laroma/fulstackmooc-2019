b5df535b84d3aecfe2445cd10038b3b29db6b608
Toni Laroma <toni.laroma@sanoma.com>
2019-04-07 23:55:47 +0300

    click handlers

--- osa1/counter/src/index.js
+++ osa1/counter/src/index.js
@@ -4,9 +4,19 @@ import ReactDOM from 'react-dom';
 const App = props => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => setCounter(counter + 1), 1000);
+  //const increaseByOne = () => setCounter(counter + 1);
+  //const setToZero = () => setCounter(0);
+  //const decreaseByOne = () => setCounter(counter - 1);
+  const setToValue = value => () => setCounter(value);
 
-  return <div>{counter}</div>;
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={setToValue(counter + 1)}>plus</button>
+      <button onClick={setToValue(0)}>zero</button>
+      <button onClick={setToValue(counter - 1)}>minus</button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
