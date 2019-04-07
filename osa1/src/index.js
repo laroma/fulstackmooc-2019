743237edea3de0c446c5c7e04e97edbd9df7435c
Toni Laroma <toni.laroma@sanoma.com>
2019-04-07 18:15:08 +0300

    props

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = () => {
+const Hello = props => {
   return (
     <div>
-      <p>Hello, World!</p>
+      <p>
+        Hello, {props.name}! You are {props.age} years old.
+      </p>
     </div>
   );
 };
 
 const App = () => {
+  const name = 'Yoda';
+  const age = 400;
+
   return (
     <div>
-      <Hello />
-      <Hello />
-      <Hello />
+      <h1>Greetings</h1>
+      <Hello name="Obi-Wan" age={22 + 25} />
+      <Hello name={name} age={age} />
     </div>
   );
 };
