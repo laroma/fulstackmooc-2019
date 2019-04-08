f5c148530f412e02f9f5253fb14bb3849dab5bc6
Toni Laroma <toni.laroma@sanoma.com>
2019-04-08 21:59:53 +0300

    step1

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const changeHandler = event => setNewName(event.target.value);
+
+  const addName = event => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <form onSubmit={addName}>
+        <div>
+          nimi: <input value={newName} onChange={changeHandler} />
+        </div>
+        <div>
+          <button type="submit">lisää</button>
+        </div>
+      </form>
+      <h2>Numerot</h2>
+      {persons.map(person => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
