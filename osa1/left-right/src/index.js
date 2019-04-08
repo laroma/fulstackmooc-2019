067886173d7ba3e159ba77c923b7da67266e3f0f
Toni Laroma <toni.laroma@sanoma.com>
2019-04-08 10:41:03 +0300

    click handlers

--- osa1/left-right/src/index.js
+++ osa1/left-right/src/index.js
@@ -1,31 +1,41 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const History = props => {
+  if (props.allClicks.length === 0) {
+    return <div>push buttons</div>;
+  }
+
+  return <div>clicks: {props.allClicks.join(' ')}</div>;
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = props => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
-  });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const handleLeftClick = () =>
-    setClicks({
-      ...clicks,
-      left: clicks.left + 1
-    });
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  };
 
-  const handleRightClick = () =>
-    setClicks({
-      ...clicks,
-      right: clicks.right + 1
-    });
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  };
 
   return (
     <div>
       <div>
-        {clicks.left}
-        <button onClick={handleLeftClick}>vasen</button>
-        <button onClick={handleRightClick}>oikea</button>
-        {clicks.right}
+        {left}
+        <Button handleClick={handleLeftClick} text="vasen" />
+        <Button handleClick={handleRightClick} text="oikea" />
+        {right}
+        <History allClicks={allClicks} />
       </div>
     </div>
   );
