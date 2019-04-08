bc5321d79489ab0c71a342b2d0d0b7ef21abec7e
Toni Laroma <toni.laroma@sanoma.com>
2019-04-08 17:31:52 +0300

    step9

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Course from './components/Course';
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack -sovelluskehitys',
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7
+        },
+        {
+          name: 'Komponenttien tila',
+          exercises: 14
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewaret',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
+
+  return (
+    <div>
+      {courses.map(course => (
+        <Course course={course} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
