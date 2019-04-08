121bd2c01286b864c15a3bf6de3517875efaf42e
Toni Laroma <toni.laroma@sanoma.com>
2019-04-08 21:02:44 +0300

    notes

--- osa2/notes/src/App.js
+++ osa2/notes/src/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Note from './components/Note';
 
-const App = ({ notes }) => {
-  const rows = () => notes.map(note => <Note key={note.id} note={note} />);
+const App = props => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
+
+  const rows = () =>
+    notesToShow.map(note => <Note key={note.id} note={note} />);
+
+  const addNote = event => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString,
+      important: Math.random() > 0.5,
+      id: notes.length + 1
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  const handleNoteChange = event => {
+    setNewNote(event.target.value);
+  };
 
   return (
     <div>
       <h1>Muistiinpanot</h1>
+      <form onSubmit={addNote}>
+        <input
+          placeholder="uusi muistiinpano..."
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">tallenna</button>
+      </form>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          näyta {showAll ? 'vain tärkeät' : 'kaikki'}
+        </button>
+      </div>
       <ul>{rows()}</ul>
     </div>
   );
