d1a74b36ece573ac9a1fad712000ba84b3e1c31c
Toni Laroma <toni.laroma@sanoma.com>
2019-04-08 22:32:31 +0300

    step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   const nameChangeHandler = event => setNewName(event.target.value);
   const numberChangeHandler = event => setNewNumber(event.target.value);
+  const searchChangeHandler = event => {
+    setSearch(event.target.value);
+    setSearchResults(
+      persons.filter(person =>
+        person.name.toLowerCase().startsWith(event.target.value.toLowerCase())
+      )
+    );
+  };
 
   const addName = event => {
     event.preventDefault();
@@ -23,9 +38,18 @@ const App = () => {
     setNewNumber('');
   };
 
+  const personList = list =>
+    list.map(person => (
+      <p key={person.name}>
+        {person.name} {person.number}
+      </p>
+    ));
+
   return (
     <div>
-      <h2>Puhelinluettelo</h2>
+      <h1>Puhelinluettelo</h1>
+      rajaa näytettäviä: <input value={search} onChange={searchChangeHandler} />
+      <h2>lisää uusi</h2>
       <form onSubmit={addName}>
         <div>
           nimi: <input value={newName} onChange={nameChangeHandler} />
@@ -38,11 +62,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numerot</h2>
-      {persons.map(person => (
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
-      ))}
+      {personList(searchResults.length > 0 ? searchResults : persons)}
     </div>
   );
 };
