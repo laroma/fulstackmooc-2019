50d039dfa273e5f7ddadabd6e2ccc31434174926
Toni Laroma <toni.laroma@sanoma.com>
2019-04-08 17:31:28 +0300

    step9

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,31 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Course from './components/Course';
-
-const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14
-      }
-    ]
-  };
-
-  return (
-    <div>
-      <Course course={course} />
-    </div>
-  );
-};
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
