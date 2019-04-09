be00e4a8ae899b736e7be1dea2a7a907a467b3ea
Toni Laroma <toni.laroma@sanoma.com>
2019-04-10 00:16:57 +0300

    step3

--- osa2/maiden-tiedot/src/App.js
+++ osa2/maiden-tiedot/src/App.js
@@ -1,63 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Input from './components/Input';
+import CountryResult from './components/CountryResult';
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [apiKey, setApiKey] = useState('');
+  const [forecastCity, setForecastCity] = useState('');
+  const [forecast, setForecast] = useState({});
 
   useEffect(() => {
     axios.get('https://restcountries.eu/rest/v2/all').then(response => {
-      console.log('response', response.data);
+      console.log(response.data);
       setCountries(response.data);
     });
   }, []);
 
-  const searchHandler = event => {
-    filterCountries(event.target.value);
-  };
+  const apiKeyHandler = event => setApiKey(event.target.value);
+  const searchHandler = event => filterCountries(event.target.value);
 
   const filterCountries = countryName => {
     const filtered = countries.filter(country =>
       country.name.toLowerCase().includes(countryName.toLowerCase())
     );
+
     setSearch(countryName);
     setSearchResults(filtered);
+
+    if (filtered.length === 1) {
+      setForecastCity(filtered[0].capital);
+      getForecast();
+    }
   };
+
+  const getForecast = () =>
+    axios
+      .get(
+        `https://api.apixu.com/v1/current.json?key=${apiKey}&q=${forecastCity}`
+      )
+      .then(response => setForecast(response.data))
+      .catch(err => {
+        console.log(err);
+      });
 
   return (
     <div>
       <div>
-        find countries <input value={search} onChange={searchHandler} />
+        <div>
+          <Input label="apixu API key" value={apiKey} handler={apiKeyHandler} />
+        </div>
+        <div>
+          <Input
+            label="find countries"
+            value={search}
+            handler={searchHandler}
+          />
+        </div>
       </div>
-      {searchResults.length === 1 ? (
-        searchResults.map(result => (
-          <div key={result.alpha3Code}>
-            <h2>{result.name}</h2>
-            <p>capital {result.capital}</p>
-            <p>population {result.population}</p>
-
-            <h3>languages</h3>
-            <ul>
-              {result.languages.map(language => (
-                <li key={language.iso639_2}>{language.name}</li>
-              ))}
-            </ul>
-            <div>
-              <img src={result.flag} alt={result.name} width="100" />
-            </div>
-          </div>
-        ))
-      ) : searchResults.length <= 10 ? (
-        searchResults.map(result => (
-          <p key={result.alpha3Code}>
-            {result.name}{' '}
-            <button onClick={() => filterCountries(result.name)}>show</button>
-          </p>
-        ))
-      ) : (
-        <p>Too many matches, specify another filter</p>
-      )}
+      <CountryResult
+        result={search ? searchResults : []}
+        forecast={forecast}
+        showCountryFn={filterCountries}
+      />
+      {apiKey === '' ? <p>Fill your apixu API key!</p> : null}
     </div>
   );
 };
