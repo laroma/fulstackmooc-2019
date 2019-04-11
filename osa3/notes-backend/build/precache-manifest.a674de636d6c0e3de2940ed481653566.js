82ffb35f748c0f63fab269d40337140599b80929
Toni Laroma <toni.laroma@sanoma.com>
2019-04-11 09:09:27 +0300

    about to deploy to heroku

---
+++ osa3/notes-backend/build/precache-manifest.a674de636d6c0e3de2940ed481653566.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4cb4a7cdda1039094b9",
+    "url": "/static/css/main.ecdfe0ac.chunk.css"
+  },
+  {
+    "revision": "e4cb4a7cdda1039094b9",
+    "url": "/static/js/main.cb0ccac2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a95d5a327b9fa556408",
+    "url": "/static/js/2.9fde0a89.chunk.js"
+  },
+  {
+    "revision": "94be9d8dca77fdd1f4585702b7d0b30d",
+    "url": "/index.html"
+  }
+];
