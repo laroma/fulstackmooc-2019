2fdb80d6b85cc719d536232accce20b23ec36bba
Toni Laroma <toni.laroma@sanoma.com>
2019-04-13 11:45:33 +0300

    using relative url

--- osa2/notes/src/services/notes.js
+++ osa2/notes/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'https://young-woodland-58992.herokuapp.com/notes';
+const baseUrl = '/notes';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
