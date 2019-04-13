6b9a830163e8e32a16161d6161e82277649622fb
Toni Laroma <toni.laroma@sanoma.com>
2019-04-13 12:46:05 +0300

    heroku

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = 'https://sleepy-wildwood-86540.herokuapp.com/api/persons';
 
 const getAll = () => {
   console.log('getAll');
