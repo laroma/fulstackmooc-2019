6093b80248f46451c03ab26cca6cfa7e525e2f2a
Toni Laroma <toni.laroma@sanoma.com>
2019-04-13 13:04:08 +0300

    front-end bundle

---
+++ fullstackmooc-2019-persons-backend/precache-manifest.04d181b62a84531348dff3c479f5abf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2e4c37572d82a9f847f",
+    "url": "/static/css/main.77c54829.chunk.css"
+  },
+  {
+    "revision": "b2e4c37572d82a9f847f",
+    "url": "/static/js/main.7ca2ac74.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a95d5a327b9fa556408",
+    "url": "/static/js/2.9fde0a89.chunk.js"
+  },
+  {
+    "revision": "d60363673cf6d240a949a335f78b845f",
+    "url": "/index.html"
+  }
+];
