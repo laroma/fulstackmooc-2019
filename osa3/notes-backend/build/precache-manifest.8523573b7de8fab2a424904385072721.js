64cc386ea946bd39e01bcaf914faf6d48abb878b
Toni Laroma <toni.laroma@sanoma.com>
2019-04-13 11:45:51 +0300

    front-end bundle

---
+++ osa3/notes-backend/build/precache-manifest.8523573b7de8fab2a424904385072721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c8a4d015e1503037f8b",
+    "url": "/static/css/main.94737016.chunk.css"
+  },
+  {
+    "revision": "8c8a4d015e1503037f8b",
+    "url": "/static/js/main.bc1d0f09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a95d5a327b9fa556408",
+    "url": "/static/js/2.9fde0a89.chunk.js"
+  },
+  {
+    "revision": "e9ce60db9ebfd7f69626b2b4b08a452b",
+    "url": "/index.html"
+  }
+];
