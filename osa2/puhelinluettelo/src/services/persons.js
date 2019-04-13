b301035b4dc461bf941b6f8f0718cb160267a221
Toni Laroma <toni.laroma@sanoma.com>
2019-04-13 13:03:57 +0300

    using relative url

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://sleepy-wildwood-86540.herokuapp.com/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   console.log('getAll');
